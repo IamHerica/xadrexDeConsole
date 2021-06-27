b5f735a781e935a479b772e6e2e104deaff7089b
Iamherica <hericacadoni@gmail.com>
2021-06-26 21:47:02 -0300

    melhorando o metodo de imprimir o tabuleiro

--- Xadrex-console/Tela.cs
+++ Xadrex-console/Tela.cs
@@ -9,15 +9,37 @@ namespace Xadrex_console
         {
             for (int linhas = 0; linhas < tab.linhas; linhas++)
             {
-                for(int colunas = 0; colunas < tab.colunas; colunas++)
+                Console.Write(8 - linhas + " ");
+                for (int colunas = 0; colunas < tab.colunas; colunas++)
                 {
                     if (tab.peca(linhas, colunas) == null)
                     {
                         Console.Write("- ");
                     }
-                    Console.Write(tab.peca(linhas, colunas) + " ");
+                    else
+                    {
+                        Tela.imprimirPeca(tab.peca(linhas, colunas));
+                        Console.Write(" ");
+                    }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+
+
+        public static void imprimirPeca(Peca peca)
+        {
+            if (peca.cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
         }
     }
